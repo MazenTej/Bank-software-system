3fb35e1ae84efe20874729f7cae2165c2c2a0305
`MazenTej` <mtej.yestunisia2016@gmail.com>
2021-05-15 22:44:29 -0400

    MasterControl is successful

--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -63,6 +63,16 @@ public class MasterControlTest {
         assertEquals("depositt 123456789 100", actual.get(1));
     }
 
+    @Test
+    void invalid_to_create_accounts_with_same_ID() {
+        input.add("create checking 12345678 1.0");
+        input.add("create checking 12345678 1.0");
+
+        List<String> actual = masterControl.start(input);
+
+        assertSingleCommand("create checking 12345678 1.0", actual);
+    }
+
     private void assertSingleCommand(String command, List<String> actual) {
         assertEquals(1, actual.size());
         assertEquals(command, actual.get(0));
