d342a385b9d0ff6b7a66c90525fcdf495fd76830
`MazenTej` <mtej.yestunisia2016@gmail.com>
2021-06-09 09:52:18 -0400

    withdraw from savings account

--- src/test/java/banking/WithdrawValidatorTest.java
+++ src/test/java/banking/WithdrawValidatorTest.java
@@ -133,8 +133,105 @@ public class WithdrawValidatorTest {
         account.setAmount(1000.11);
         boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 300.15");
         assertTrue(actual);
+    }
+
+    @Test
+    void withdraw_from_savings_account_amount_in_dollars_more_than_maximum_is_invalid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 1200");
+        assertFalse(actual);
 
     }
 
+    @Test
+    void withdraw_from_savings_account_amount_in_dollars_and_cents_more_than_maximum_is_invalid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 10000.12");
+        assertFalse(actual);
+    }
+
+    @Test
+    void withdraw_from_savings_account_negative_amount_in_dollars_is_invalid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 -500");
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void withdraw_from_savings_account_negative_amount_in_dollars_and_cents_is_invalid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 -500.12");
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void withdraw_from_savings_account_zero_amount_is_valid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 0");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void withdraw_from_savings_account_equals_to_maximum_amount_is_valid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 1000");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void withdraw_from_savings_account_amount_in_dollars_in_range_is_valid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 300");
+        assertTrue(actual);
+    }
+
+    @Test
+    void withdraw_from_savings_account_amount_in_dollars_and_cents_in_range_is_valid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 300.15");
+        assertTrue(actual);
+    }
+
+    @Test
+    void withdraw_from_savings_once_a_month_is_valid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000.11);
+        bank.passTime(1);
+        boolean actual = withdrawValidator.validateWithdraw("withdraw 12345678 300.15");
+        assertTrue(actual);
+    }
+
+    @Test
+    void withdraw_from_savings_account_more_than_once_a_month_is_invalid() {
+        bank.addSavingsAccount("12345678", 0.6);
+        Account account = bank.retrieveAccount("12345678");
+        account.setAmount(1000);
+        bank.passTime(1);
+        boolean actual1 = withdrawValidator.validateWithdraw("withdraw 12345678 300.15");
+        boolean actual2 = withdrawValidator.validateWithdraw("withdraw 12345678 300.15");
+        assertFalse(actual2);
+        assertTrue(actual1);
+    }
 
 }
